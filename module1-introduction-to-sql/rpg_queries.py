2e13d15c8543585bc678c0e2905f3ce8424dcad3
ocalzada <ocalzada@users.noreply.github.com>
2019-10-09 14:25:00 -0600

    update rpg_queries, titanic.py

--- module1-introduction-to-sql/rpg_queries.py
+++ module1-introduction-to-sql/rpg_queries.py
@@ -33,18 +33,31 @@ query7 = 'SELECT COUNT (*) FROM armory_item WHERE item_id BETWEEN 138 AND 174;'
 query8 = 'SELECT COUNT (*) FROM armory_item WHERE item_id NOT BETWEEN 138 AND 174;'
 # 137 'items' are not weapons
 
-query9 = 'SELECT ccc.character_id, ccc.name, ccc.exp 
-            FROM charactercreator_character AS ccc 
-            INNER JOIN charactercreator_character_inventory as cci 
-            ON ccc.character_id = cci.item_id LIMIT 20;'
+query9 = 
+'''SELECT ccc.character_id, ccc.name, ccc.exp 
+FROM charactercreator_character AS ccc 
+INNER JOIN charactercreator_character_inventory as cci 
+ON ccc.character_id = cci.item_id LIMIT 20;'''
 
 # - How many Weapons does each character have? (Return first 20 rows)
+query10 = 
+'''SELECT ccc.character_id, ccc.name, ccc.exp, ai.name, aw.power
+ FROM charactercreator_character AS ccc,
+ charactercreator_character_inventory AS cci,
+ armory_item AS ai, armory_weapon AS aw
+ WHERE ccc.character_id = cci.character_id
+ AND cci.item_id = ai.item_id
+ AND ai.item_id = aw.item_ptr_id
+ LIMIT 20;'''
+Code above conducts an implicit join among four tables.
+Returns all the weapons for each characters
+Theres a total of 203 weapons.
 
 # - On average, how many Items does each Character have?
-
-# 898 items / 174 characters = 5 items/characters
+898 items / 174 characters = 5 items/characters
 
 # - On average, how many Weapons does each character have?
+203 weapons / 174 characters = ~1 weapon/character
 
 curs.close()
 conn.commit()
